83d8fa15e00a34e5c017c5b84792b564079ce46f
John Adams Jr <john.c.adams32@gmail.com>
2021-12-09 20:37:57 -0700

    Premliminary project with task list, game art, and game data

---
+++ higher_lower.py
@@ -0,0 +1,14 @@
+# Task list
+# import logo
+# import random
+# select two random entities
+# Present the logo to the user
+# present the first entity to the user
+# present the vs logo to the user
+# present the second entity to the user
+# ask the user for input choosing either the first or second entity
+# If the user gets it correct, move the second entity to the first, and add a new
+# entity to the second.
+# If the user gets it wrong end the game
+# Allow the user to keep guessing until they get it wrong
+# keep track of how many times the user has gotten it right after logo
