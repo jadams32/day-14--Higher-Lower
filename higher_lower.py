26f4d86d3553238c2488e28febf93d39b52b7b83
John Adams Jr <john.c.adams32@gmail.com>
2021-12-11 09:52:12 -0700

    basic task list complete functions needed, refactoring needed

--- higher_lower.py
+++ higher_lower.py
@@ -1,14 +1,58 @@
 # Task list
 # import logo
+from game_art import logo
+from game_art import vs
+
 # import random
+import random
+#import game data
+from game_data import data
+
 # select two random entities
+entity_a = random.choice(data)
+entity_b = random.choice(data)
+
 # Present the logo to the user
-# present the first entity to the user
-# present the vs logo to the user
-# present the second entity to the user
-# ask the user for input choosing either the first or second entity
-# If the user gets it correct, move the second entity to the first, and add a new
-# entity to the second.
-# If the user gets it wrong end the game
+print(logo)
+
+
+print(f'Compare A: {entity_a.get("name")}, {entity_a.get("description")}, from {entity_a.get("country")}')
+print(vs)
+print(f'Against B: {entity_b.get("name")}, {entity_b.get("description")}, from {entity_b.get("country")}')
+choice = input('Who has more followers? Type "A" or "B":\n').lower()
+
+def play_again():
+    pass
+
+score = 0
+
+if choice == "a" and entity_a.get("follower_count") > entity_b.get("follower_count"):
+    score += 1
+    print(f"You are correct! Your current score is {score}")
+    entity_a = entity_b
+    entity_b = random.choice(data)
+
+elif choice == "b" and entity_b.get("follower_count") > entity_a.get("follower_count"):
+    score += 1
+    print(f"You are correct! Your current score is {score}")
+    entity_a = entity_b
+    entity_b = random.choice(data)
+
+elif choice == "a" and entity_a.get("follower_count") < entity_b.get("follower_count"):
+    print(f"That is incorrect! You lose.")
+    play_again()
+
+else:
+    print(f"That is incorrect! You lose.")
+    play_again()
+
+print(entity_a.get("follower_count"))
+print(entity_b.get("follower_count"))
+print(entity_a)
+print(entity_b)
+
+# Task list
+# places different aspects of the game into functions
 # Allow the user to keep guessing until they get it wrong
-# keep track of how many times the user has gotten it right after logo
+# Refractor code
+# Comment code
